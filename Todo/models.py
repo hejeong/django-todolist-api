f6ee4f29010e0a6391c10703fbfa6572d955ad43
hejeong <jonhong777@gmail.com>
2020-05-21 14:20:11 -0400

    Add Todo app to settings file, migrate Todo model, and make viewable on admin site

--- Todo/models.py
+++ Todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    memo = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(null=True, blank=True)
+    # add user relation
+
+    def __str__(self):
+        return self.title
