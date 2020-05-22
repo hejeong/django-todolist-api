820a281a0d980003bd7dce45af4c3ddffb61783f
hejeong <jonhong777@gmail.com>
2020-05-22 11:43:27 -0400

    Add create_todo fixture and added tests for Todo detail endpoints (retrieve, update, delete)

--- Todo/tests/test_todo_endpoints.py
+++ Todo/tests/test_todo_endpoints.py
@@ -1,11 +1,24 @@
 import pytest
 
 from django.urls import reverse
+
 from Todo.models import Todo
 
+from rest_framework import status
 
-@pytest.mark.django_db
-def test_todo_list(client):
+
+@pytest.fixture
+def create_todo(db):
+    """
+    Fixture to make a new todo object
+    """
+
+    def make_todo(**kwargs):
+        return Todo.objects.create(**kwargs)
+    return make_todo
+
+
+def test_todo_list(db, client, create_todo):
     """
     Test that a list of todos are returned when GET request is made to '/api/todos'
     - @param client is a built-in fixture from pytest-django based off of django.test.client /
@@ -13,22 +26,20 @@ def test_todo_list(client):
     """
     
     # create a Todo object in db
-    Todo.objects.create(
-        title="Learn how to use pytest",
-        memo="Use the book 'Python Testing with Pytest'",
-    )
+    todo = create_todo(title="Learn how to use pytest", memo="Use the book 'Python Testing with Pytest'")
+    
     # get the url to for getting the todo list
     url = reverse('todo-list-or-create')
     # returns the response object from endpoint
     response = client.get(url)
-    assert response.status_code == 200
-    assert response.json()[0]['title'] == "Learn how to use pytest"
+    
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()[0]['title'] == todo.title
 
 
-@pytest.mark.django_db
-def test_todo_list(client):
+def test_todo_create(db, client):
     """
-    Test that a new object is successfully created when POST request is made to '/api/todos'
+    Test that a new object is successfully created when POST request is made to '/api/todos' successfully
     """
     
     # should begin with zero Todo objects
@@ -36,7 +47,7 @@ def test_todo_list(client):
     # define model values
     title = "Learn how to use pytest"
     memo = "Use the book 'Python Testing with Pytest'"
-    # get the url to for getting the todo list
+    # get the url to for creating the todo object
     url = reverse('todo-list-or-create')
     # returns the response object from endpoint
     response = client.post(
@@ -45,7 +56,67 @@ def test_todo_list(client):
                                 'title':title, 
                                 "memo":memo,
                             })
-    assert response.status_code == 200
+                            
+    assert response.status_code == status.HTTP_201_CREATED
     assert Todo.objects.count() == 1
     assert Todo.objects.all()[0]['title'] == title
     assert Todo.objects.all()[0]['memo'] == memo
+
+
+def test_todo_detail(db, client, create_todo):
+    """
+    Test that a get request can be made to '/api/todos/<id>' to obtain the detail page of a todo object successfully
+    """
+
+    # create todo object in db
+    todo = create_todo(title='Learn how to use pytest', memo="Use the book 'Python Testing with Pytest'")
+    # get url for retrieving detail page for specific blog
+    url = reverse('todo-detail-get-update-delete', args=(todo.pk,))
+    response = client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()[0]['id'] == todo.pk
+
+
+def test_todo_update(db, client, create_todo):
+    """
+    Test that a PUT request can be made to '/api/todos/<id>' to update a todo object successfully
+    """
+
+    # create todo object in db
+    todo = create_todo(title='Learn how to use pytest', memo="Use the book 'Python Testing with Pytest'")
+    # set values to be updated
+    new_title = 'Learn how to use pytest with DjangoRestFramework'
+    new_memo = "Use the book 'Python Testing with Pytest and other resources'"
+    # get url for updating the todo object
+    url = reverse('todo-detail-get-update-delete', args=(todo.pk,))
+    response = client.put(
+                        url, 
+                        {
+                            'title':new_title,
+                            'memo':new_memo,
+                        })
+    assert response.status_code == status.HTTP_202_ACCEPTED
+
+    updated_todo = Todo.objects.get(pk=todo.pk)
+    assert updated_todo.title == new_title
+    assert updated_todo.memo == new_memo
+
+
+def test_todo_delete(db, client, create_todo):
+    """
+    Test that a DELETE request can be made to '/api/todos/<id>' to destroy a todo object successfully
+    """
+
+    # create todo object in db
+    todo = create_todo(title='Learn how to use pytest', memo="Use the book 'Python Testing with Pytest'")
+    # assert there are exactly 1 Todo objects
+    assert Todo.objects.count() == 1
+
+    # get url for updating the todo object
+    url = reverse('todo-detail-get-update-delete', args=(todo.pk,))
+    # make delete request to '/api/todos/<id>'
+    response = client.delete(url)
+    
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert Todo.objects.count() == 0
