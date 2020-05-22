54c3cb56326094c5584413b8988de0c3a71c0e98
hejeong <jonhong777@gmail.com>
2020-05-22 14:03:17 -0400

    Create TodoViewSet

--- Todo/views.py
+++ Todo/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    """
+    This provides 'list', 'create', 'retrieve', 'update' and 'destroy actions for Todo
+    """
+
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
