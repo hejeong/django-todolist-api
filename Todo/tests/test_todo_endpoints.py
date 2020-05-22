56564f9599e26f46f0d9c769ba4edd767f405a12
hejeong <jonhong777@gmail.com>
2020-05-22 14:20:44 -0400

    Change tests to reflect url names generated by router

--- Todo/tests/test_todo_endpoints.py
+++ Todo/tests/test_todo_endpoints.py
@@ -29,7 +29,7 @@ def test_todo_list(db, client, create_todo):
     todo = create_todo(title="Learn how to use pytest", memo="Use the book 'Python Testing with Pytest'")
     
     # get the url to for getting the todo list
-    url = reverse('todo-list-or-create')
+    url = reverse('todo-list')
     # returns the response object from endpoint
     response = client.get(url)
     
@@ -48,7 +48,7 @@ def test_todo_create(db, client):
     title = "Learn how to use pytest"
     memo = "Use the book 'Python Testing with Pytest'"
     # get the url to for creating the todo object
-    url = reverse('todo-list-or-create')
+    url = reverse('todo-list')
     # returns the response object from endpoint
     response = client.post(
                             url, 
@@ -71,7 +71,7 @@ def test_todo_detail(db, client, create_todo):
     # create todo object in db
     todo = create_todo(title='Learn how to use pytest', memo="Use the book 'Python Testing with Pytest'")
     # get url for retrieving detail page for specific blog
-    url = reverse('todo-detail-get-update-delete', args=(todo.pk,))
+    url = reverse('todo-detail', args=(todo.pk,))
     response = client.get(url)
 
     assert response.status_code == status.HTTP_200_OK
@@ -89,7 +89,7 @@ def test_todo_update(db, client, create_todo):
     new_title = 'Learn how to use pytest with DjangoRestFramework'
     new_memo = "Use the book 'Python Testing with Pytest and other resources'"
     # get url for updating the todo object
-    url = reverse('todo-detail-get-update-delete', args=(todo.pk,))
+    url = reverse('todo-detail', args=(todo.pk,))
     response = client.put(
                         url, 
                         {
@@ -114,7 +114,7 @@ def test_todo_delete(db, client, create_todo):
     assert Todo.objects.count() == 1
 
     # get url for updating the todo object
-    url = reverse('todo-detail-get-update-delete', args=(todo.pk,))
+    url = reverse('todo-detail', args=(todo.pk,))
     # make delete request to '/api/todos/<id>'
     response = client.delete(url)
     
