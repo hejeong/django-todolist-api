8392fa0d623161341766f45485d3ca804372532e
hejeong <jonhong777@gmail.com>
2020-05-22 17:23:56 -0400

    Add authentication requirement for TodoViewSet

--- Todo/views.py
+++ Todo/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Todo
 from .serializers import TodoSerializer
@@ -9,5 +10,7 @@ class TodoViewSet(viewsets.ModelViewSet):
     This provides 'list', 'create', 'retrieve', 'update' and 'destroy actions for Todo
     """
 
+    permission_classes = [IsAuthenticated]
+    
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
