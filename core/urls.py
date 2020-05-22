340249ad9f7540b6267c13feff2e89ff5a1c2365
hejeong <jonhong777@gmail.com>
2020-05-22 17:22:49 -0400

    Add jwt authentication routes

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt import views as jwt_views
+
 from Todo.views import TodoViewSet
 
 
@@ -9,4 +12,6 @@ router.register(r'todos', TodoViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('token/refresh', jwt_views.TokenRefreshView.as_view(), name='token-refresh'),
 ]
