b1b1f6dde915a2bc73001e29bf7a2b55633afac2
hejeong <jonhong777@gmail.com>
2020-06-02 18:34:29 -0400

    Add env-1 to allowed hosts

--- todolist_api/settings.py
+++ todolist_api/settings.py
@@ -137,3 +137,5 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(minutes=2),
 }
+
+STATIC_ROOT = "/var/www/Whattodo-env-1.eba-wey5cqsv.us-east-1.elasticbeanstalk.com/static/"
