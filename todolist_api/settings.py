ff545c546aaee3384fab18044b08da56621c5824
hejeong <jonhong777@gmail.com>
2020-06-05 17:06:24 -0400

    Update django.config

--- todolist_api/settings.py
+++ todolist_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '4yhq()car+3c4upom6mk57v(t3_0-=wv-bzda_xdl-w5kcds@d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'Whattodo-env-1.eba-wey5cqsv.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['localhost', 'Whattodoapp-env.eba-jpp393pq.us-east-1.elasticbeanstalk.com']
 
 
 # Application definition
