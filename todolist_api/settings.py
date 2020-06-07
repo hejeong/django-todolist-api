89c6350674c1b1e7b1caec704722ae944db5b334
hejeong <jonhong777@gmail.com>
2020-06-07 17:49:01 -0400

    Update django requirements

--- todolist_api/settings.py
+++ todolist_api/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
@@ -138,4 +139,3 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(minutes=2),
 }
 
-STATIC_ROOT = "/var/www/Whattodoapp-env.eba-jpp393pq.us-east-1.elasticbeanstalk.com/static/"
