df4a7c60fc48f8b181e874ca80f4c5ac4b20214a
hejeong <jonhong777@gmail.com>
2020-06-08 09:39:30 -0400

    Update django requirements

--- todolist_api/settings.py
+++ todolist_api/settings.py
@@ -123,8 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
