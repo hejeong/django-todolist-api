a4d90915588588e02a3eaf285f5f0b3ca2b2ca8d
hejeong <jonhong777@gmail.com>
2020-06-08 11:01:47 -0400

    Update django requirements

--- todolist_api/settings.py
+++ todolist_api/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
